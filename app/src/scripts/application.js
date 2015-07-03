8a18afe797bdf00b23cd9a6d42d205f3380aee5a
Leandro Martins <leandrodmax@gmail.com>
2015-07-03 19:39:10 -0300

    Meviento ao logo

--- app/src/scripts/application.js
+++ app/src/scripts/application.js
@@ -7,5 +7,15 @@ $(function(){
       $(panel).slideToggle('slow');
     }
   })
+  $('li a').bind('click', function(event) {
+      var $anchor = $(this);
+      event.preventDefault();
 
+      console.log( $($($anchor).attr('href')).offset().top);
+/*
+      $('html, body').stop().animate({
+          scrollTop: $($($anchor).attr('href')).offset().top
+      }, 1500, 'easeInOutExpo');
+*/
+  });
 });
