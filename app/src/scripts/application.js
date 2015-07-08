9c14df4a4fcaba30068650159786f6a89dfc156b
Leandro Martins <leandrodmax@gmail.com>
2015-07-08 12:39:15 -0300

    Alter Images on click

--- app/src/scripts/application.js
+++ app/src/scripts/application.js
@@ -7,13 +7,11 @@ $(function(){
       var href = $(this).attr('href');
 
       panel.slideToggle('slow', function(){
-        if(!panel.is(":visible")){
-          scrollTo(href);
-        }
+
+        console.log(href);
+        scrollTo(href);
       });
-
       return false;
-
     }
   })
   $('li a').bind('click', function(event) {
@@ -31,18 +29,33 @@ $(function(){
     click: function(e){
       e.preventDefault();
       var fruit =  $(this).data('fruit');
+      var main = $('.produto-sabores .main');
+      var person = $('#persona');
+      var persons = $('#personas');
       var title = $("#speak > h2");
       var description = $("#speak > p");
 
+      main.removeClass('limao tangerina amora pessego maracuja morango');
+      main.addClass(fruit);
+
+      $('.personas').find(':hidden').show();
+      $(this).hide();
+
       switch (fruit) {
         case 'limao':
+          person.children().eq(0).attr('src', 'public/images/persona_01.png');
+          person.children().eq(1).html('Limão Siciliano');
           title.html('Se a vida te der um limão, torça para que seja Siciliano.');
-          description.html('Ma che! Sou o <stron>Limão Siciliano</stron>. Sou originário da Ásia, mas fiz minha fama na Itália, na região da Sicilia, <br>
-          quando os árabes me trouxeram há muuuuuito tempo. Sou bonito, amarelo, cheiroso e cheinho de suco. No Brasil <br>
-          ainda são poucos lugares que me cultivam, então aproveite para sentir todo o meu sabor nas balas DuBalaco!');
-
+          description.html('Ma che! Sou o <stron>Limão Siciliano</stron>.
+          Sou originário da Ásia, mas fiz minha fama na Itália, na região
+          da Sicilia, <br> quando os árabes me trouxeram há muuuuuito tempo.
+          Sou bonito, amarelo, cheiroso e cheinho de suco. No Brasil <br>
+          ainda são poucos lugares que me cultivam, então aproveite para sentir
+          todo o meu sabor nas balas DuBalaco!');
         break;
         case 'tangerina':
+          person.children().eq(0).attr('src', 'public/images/persona_02.png');
+          person.children().eq(1).html('Tangerina');
           title.html('Sou uma delícia, não é à toa que estou sempre dizendo hummmmmm.');
           description.html('Vim da Ásia, mas meu nome significa laranja de
           Tânger, porto no Marrocos onde eu era comercializada. No Brasil sou
@@ -51,10 +64,18 @@ $(function(){
           minerais e quem provar também vai dizer hummmmm. ')
         break;
         case 'amora':
-          title.html('amora');
-          description.html('amora');
+          person.children().eq(0).attr('src', 'public/images/persona_03.png');
+          person.children().eq(1).html('Amora');
+          title.html('Eu sou a amora, docinha e nutritiva, não tem quem não me adora ');
+          description.html('Minha origem é asiática, mas me adaptei tão bem ao
+          Brasil que posso dizer que tenho aura (quer dizer polpa) brasileira.
+          Apesar de pequenininha trago um monte de benefícios, como vitaminas
+          A e C, antioxidantes, ferro, magnésio e zinco. Com tudo isso, não
+          poderia ficar de fora da seleção de frutas escolhidas pela DuBalaco.');
         break;
         case 'pessego':
+          person.children().eq(0).attr('src', 'public/images/persona_04.png');
+          person.children().eq(1).html('Pêssego');
           title.html('Sabor milenar para você curtir no seu dia a dia');
           description.html('Sou nativo da China, mas durante muito tempo
           achavam que eu era da Pérsia e me chamavam de malus persicum
@@ -62,10 +83,11 @@ $(function(){
           20 séculos A.C. Apesar da idade, sou famoso por retardar o
           envelhecimento devido à grande quantidade de antioxidantes,
           betacaroteno e vitaminas C e B12. Tenho tudo para fazer sucesso
-          com os novinhos e com os mais velhinhos.
-');
+          com os novinhos e com os mais velhinhos.');
         break;
         case 'maracuja':
+          person.children().eq(0).attr('src', 'public/images/persona_05.png');
+          person.children().eq(1).html('Maracujá');
           title.html('Olá, sou um craque brasileiro, o Maracujá');
           description.html('Nascido e criado aqui mesmo, sou, como todos,
           apaixonado pelo futebol. Isso fica evidente no meu formato de bola e
@@ -73,6 +95,8 @@ $(function(){
           fico amarelinho e pronto para me transformar em um delicioso suco.');
         break;
         case 'morango':
+          person.children().eq(0).attr('src', 'public/images/persona_06.png');
+          person.children().eq(1).html('Morango');
           title.html('Vim da França, mas moro no coração de todo mundo. ');
           description.html('Nasci na Bretanha, no noroeste da França e faço
           sucesso por onde eu passo. Minha cor vermelha e o formato de coração
@@ -82,8 +106,9 @@ $(function(){
           tratam com o devido respeito e posso dizer sem modéstia que, além de
           tudo, ainda sou orgânico.');
         break;
-
       }
+
+      scrollTo('#sabores');
 
     }
   })
